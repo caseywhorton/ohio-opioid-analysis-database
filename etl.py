35e7edefc13fb4d79b3772eb9352c4f329cd58ea
Casey Whorton <27929027+caseywhorton@users.noreply.github.com>
2021-11-27 08:50:21 -0500

    Update etl.py

--- etl.py
+++ etl.py
@@ -53,6 +53,9 @@ def main():
     cur.execute(sql_queries.copy_county_raw.format(params['county_raw_s3'], params['iam_role']))
     conn.commit()
     
+    # data quality check that table is not empty
+    helper.check_greater_than_zero("county_raw", cur, conn)
+    
     # copy the bureau of labor statistics data to tables on Redshift
     bls_dict = dict(zip(sql_queries.bls_table_list,sql_queries.bls_file_list))
 
@@ -62,6 +65,9 @@ def main():
                                              params['iam_role'])
         cur.execute(query)
         conn.commit()
+        
+        # data quality check that table is not empty
+        helper.check_greater_than_zero(key, cur, conn)
     
     print('Calling APIs and inserting data into database...')
     
@@ -74,6 +80,9 @@ def main():
         cur.execute(sql_queries.insert_table_unemployment_rate, list(row))
     
     conn.commit()
+ 
+    # data quality check that table has number of expected rows
+    helper.check_expected_rows("unemployment_rate", cur, conn, len(df))
     
     # Ohio candidate query from opensecrets
     
@@ -85,15 +94,23 @@ def main():
     df_list = list()
     for result in tqdm(ohio_candidates, desc = 'candIndByInd'):
         cid, year = result
-        df = helper.opensecrets_candindbyind(cid,year,'H04', params['opensecrets_api_key'])
-        df.drop('cand_name',axis=1,inplace=True)
-        df_list.append(df)
+        try:
+            df = helper.opensecrets_candindbyind(cid,year,'H04', params['opensecrets_api_key'])
+            df.drop('cand_name',axis=1,inplace=True)
+            df_list.append(df)
+        except:
+            pass
        
     df = pd.concat(df_list)
     
     for i, row in tqdm(df.iterrows(), total = len(df), desc = 'Inserting Rows to candindbyind'):
         cur.execute(sql_queries.insert_table_candindbyind, list(row))
         
+    conn.commit()
+    
+    # data quality check that table has number of expected rows
+    helper.check_expected_rows("candindbyind", cur, conn, len(df))
+    
     # candsummary from opensecrets
     
     df_list = list()
@@ -102,11 +119,11 @@ def main():
         df = helper.opensecrets_candsummary(cid,year, params['opensecrets_api_key'])
         try:
             df = df.replace('',0)
+            df.drop('cand_name',axis=1,inplace=True)
+            df_list.append(df)
         except:
             pass
-        df.drop('cand_name',axis=1,inplace=True)
-        df_list.append(df)
-       
+        
     df = pd.concat(df_list)
     
     for i, row in tqdm(df.iterrows(), total = len(df), desc = 'Inserting Rows to candsummary'):
@@ -114,14 +131,20 @@ def main():
      
     conn.commit()
     
+    # data quality check that table has number of expected rows
+    helper.check_expected_rows("candsummary", cur, conn, len(df))
+    
     # candcontrib from opensecrets
     
     df_list = list()
     for result in tqdm(ohio_candidates, desc = 'candcontrib'):
         cid, year = result
         df = helper.opensecrets_candcontrib(cid, year, params['opensecrets_api_key'])
-        df.drop('cand_name',axis=1,inplace=True)
-        df_list.append(df)
+        try:
+            df.drop('cand_name',axis=1,inplace=True)
+            df_list.append(df)
+        except:
+            pass
        
     df = pd.concat(df_list)
     
@@ -129,6 +152,9 @@ def main():
         cur.execute(sql_queries.insert_table_candcontrib, list(row))
     
     conn.commit()
+    
+    # data quality check that table has number of expected rows
+    helper.check_expected_rows("candcontrib", cur, conn, len(df))
     
     # county list of ohio counties from arcos dataset
     
@@ -139,6 +165,9 @@ def main():
         cur.execute(sql_queries.insert_table_ohio_county, list(row))
     
     conn.commit()
+    
+    # data quality check that table has number of expected rows
+    helper.check_expected_rows("ohio_county", cur, conn, len(ohio_county_df))
     
     # county population from arcos dataset
     
@@ -158,6 +187,9 @@ def main():
      
     conn.commit()
     
+    # data quality check that table has number of expected rows
+    helper.check_expected_rows("county_pop", cur, conn, len(df))
+    
     # drug list table from arcos dataset
     
     df = helper.drug_list(key=params['arcos_api_key'])
@@ -166,6 +198,9 @@ def main():
         cur.execute(sql_queries.insert_table_drug_list, list(row))
        
     conn.commit()
+    
+    # data quality check that table has number of expected rows
+    helper.check_expected_rows("drug_list", cur, conn, len(df))
     
     # pharmacy location data from arcos dataset
     
@@ -185,6 +220,9 @@ def main():
     
     conn.commit()
     
+    # data quality check that table has number of expected rows
+    helper.check_expected_rows("pharm_location", cur, conn, len(df))
+    
     # buyer address from arcos dataset
     
     df_list = list()
@@ -203,6 +241,9 @@ def main():
         
     conn.commit()
     
+    # data quality check that table has number of expected rows
+    helper.check_expected_rows("buyer_address", cur, conn, len(df))
+    
     # reporter address from arcos dataset
     
     df_list = list()
@@ -220,6 +261,9 @@ def main():
         cur.execute(sql_queries.insert_table_reporter_address, list(row))
         
     conn.commit()
+    
+    # data quality check that table has number of expected rows
+    helper.check_expected_rows("reporter_address", cur, conn, len(df))
     
 if __name__ == "__main__":
     main()
